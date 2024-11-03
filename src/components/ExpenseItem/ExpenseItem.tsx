038accc2de91732e555deff413c0cb9491e54bda
X3ros7 <a.a.prosv2005@gmail.com>
2024-11-03 11:54:33 +0200

    Format ExpenseItem.tsx

--- src/components/ExpenseItem/ExpenseItem.tsx
+++ src/components/ExpenseItem/ExpenseItem.tsx
@@ -1,8 +1,8 @@
 import styles from "./ExpenseItem.module.css";
 import ExpenseDate from "../ExpenseDate";
-import { IExpense } from "../Expenses";
+import {IExpense} from "../Expenses";
 import {db} from "../../firebase";
-import {doc, deleteDoc} from "firebase/firestore";
+import {deleteDoc, doc} from "firebase/firestore";
 import {SetStateAction} from "react";
 
 interface ExpenseItemProps {
@@ -10,28 +10,27 @@ interface ExpenseItemProps {
     setExpenses: (value: SetStateAction<IExpense[]>) => void
 }
 
-const ExpenseItem = ({ expense, setExpenses }: ExpenseItemProps) => {
+const ExpenseItem = ({expense, setExpenses}: ExpenseItemProps) => {
     const onDeleteHandler = async (expenseId: string) => {
         try {
             const expensesRef = doc(db, "expenses", expenseId);
             await deleteDoc(expensesRef);
             setExpenses((prevExpenses) => prevExpenses.filter((expense) => expense.id !== expenseId));
-        }
-        catch (e) {
+        } catch (e) {
             console.log(e)
         }
     }
 
     return (
         <div className={styles.expense_item}>
-            <ExpenseDate date={expense.date} />
+            <ExpenseDate date={expense.date}/>
 
             <h2>{expense.title}</h2>
             <div className={styles.expense_item__price}>${expense.amount}</div>
-            <img src="delete.svg" alt="test" style={{
+            <img src="delete.svg" alt="delete" style={{
                 marginLeft: "10px",
                 marginRight: "10px",
-            }} onClick={() => onDeleteHandler(expense.id)} />
+            }} onClick={() => onDeleteHandler(expense.id)}/>
         </div>
     );
 };
